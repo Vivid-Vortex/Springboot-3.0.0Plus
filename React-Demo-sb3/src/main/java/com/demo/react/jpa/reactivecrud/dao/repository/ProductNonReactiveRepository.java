d5d9e5c18557215c7c027e35c71719a3b8cc9d5e
Vivid-Vortex <ctntc9.dpk@gmail.com>
2024-02-07 13:26:39 +0530

    modified entity classes and repos
    `modified entity classes and repos

--- React-Demo-sb3/src/main/java/com/demo/react/jpa/reactivecrud/dao/repository/ProductNonReactiveRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.demo.react.jpa.reactivecrud.dao.repository;
-//
-//import com.demo.react.jpa.reactivecrud.dao.entity.Product;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface ProductNonReactiveRepository extends JpaRepository<Product, Long> {
-//}
-//
